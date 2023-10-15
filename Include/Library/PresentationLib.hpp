2beed99d6db6665733e2b59aba8305886c646721
ndifix <yoshiki.linuxmint@gmail.com>
2023-10-15 14:15:26 +0900

    presentation properties partを追加

--- Include/Library/PresentationLib.hpp
+++ Include/Library/PresentationLib.hpp
@@ -6,9 +6,27 @@
 #include <Library/ThemeLib.hpp>
 #include <Library/XmlFileLib.hpp>
 
+class PresentationPropertiesPart {
+private:
+    xmlFile::PresentationProperties presentationProperties;
+public:
+    const std::string rootDir;
+    const std::string partDir;
+
+    PresentationPropertiesPart (
+        const std::string &root,
+        std::string dir
+        );
+
+    Status
+    Write (
+        ) const;
+};
+
 class PresentationPart {
 private:
     xmlFile::Presentation presentation;
+    PresentationPropertiesPart *presPropPart = nullptr;
     ThemePart *themePart = nullptr;
 public:
     const std::string rootDir;
