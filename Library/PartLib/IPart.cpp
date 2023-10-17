72efeaaf03aab19a24fd9c1f21c219cd0f88caa6
ndifix <yoshiki.linuxmint@gmail.com>
2023-10-17 19:54:02 +0900

    IPartのポインターをスマートにした

--- Library/PartLib/IPart.cpp
+++ Library/PartLib/IPart.cpp
@@ -10,17 +10,6 @@ IPart::IPart (
 {
 }
 
-IPart::~IPart (
-    )
-{
-    while (!this->childParts.empty()) {
-        if (this->childParts.back() != nullptr) {
-            delete this->childParts.back();
-        }
-        this->childParts.pop_back();
-    }
-}
-
 std::string
 IPart::GetXmlFilePath (
     ) const
@@ -34,7 +23,7 @@ IPart::GetXmlFilePath (
 
 void
 IPart::AddChildPart (
-    IPart *newPart
+    std::shared_ptr<IPart> newPart
     )
 {
     this->childParts.push_back(newPart);
