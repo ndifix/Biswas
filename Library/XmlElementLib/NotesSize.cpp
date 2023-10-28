993cb9f0df0329b48d995fdfd3d6ef7f25535e7a
ndifix <yoshiki.linuxmint@gmail.com>
2023-10-28 21:30:17 +0900

    プレゼンテーションにnotesSizeとdefaultTextStyleを追加

---
+++ Library/XmlElementLib/NotesSize.cpp
@@ -0,0 +1,14 @@
+#include <Biswas.hpp>
+#include <Library/XmlElementLib.hpp>
+
+using namespace xmlElm;
+
+void
+NotesSize::Write (
+    std::ofstream &ofs
+    )
+{
+    this->AddAttribute("cx", std::to_string(this->Cx));
+    this->AddAttribute("cy", std::to_string(this->Cy));
+    XmlElement::Write(ofs);
+}
