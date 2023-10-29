08b9fd4af04cb7af5db0d85f999cb3946754624f
ndifix <yoshiki.linuxmint@gmail.com>
2023-10-29 16:53:02 +0900

    shape treeを追加

---
+++ Include/Library/XmlElementLib/ShapeTree.hpp
@@ -0,0 +1,16 @@
+#ifndef XML_ELEMENT_LIB_SHAPETREE
+#define XML_ELEMENT_LIB_SHAPETREE
+
+#include <Library/XmlBaseLib.hpp>
+
+namespace xmlElm {
+
+class ShapeTree : public XmlElement {
+private:
+public:
+    inline ShapeTree() : XmlElement("ShapeTree", xmlns::presenta) {};
+};
+
+}
+
+#endif
