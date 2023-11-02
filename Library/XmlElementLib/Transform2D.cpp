de984f43d7717e7eb3d93a0847793d7045b17d39
ndifix <yoshiki.linuxmint@gmail.com>
2023-11-02 20:30:11 +0900

    ChildOffsetを追加

--- Library/XmlElementLib/Transform2D.cpp
+++ Library/XmlElementLib/Transform2D.cpp
@@ -5,8 +5,10 @@ using namespace xmlElm;
 Transform2D::Transform2D (
     ) : XmlElement("xfrm", xmlns::drawingm)
 {
+    this->childOffset = new ChildOffset();
     this->extents = new Extents();
     this->offset = new Offset();
     this->AddChildElement(this->offset);
     this->AddChildElement(this->extents);
+    this->AddChildElement(this->childOffset);
 }
