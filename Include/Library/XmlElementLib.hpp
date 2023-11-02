626b5bf43ebbb8aa3c9e1ec226447a954bdc0af0
ndifix <yoshiki.linuxmint@gmail.com>
2023-11-02 19:03:54 +0900

    XmlElementLibは全てincludeするようにした

--- Include/Library/XmlElementLib.hpp
+++ Include/Library/XmlElementLib.hpp
@@ -4,12 +4,16 @@
 #include <Library/XmlElementLib/CommonSlideData.hpp>
 #include <Library/XmlElementLib/Default.hpp>
 #include <Library/XmlElementLib/DefaultTextStyle.hpp>
+#include <Library/XmlElementLib/GroupShapeProperties.hpp>
 #include <Library/XmlElementLib/NonVisualDrawingProperties.hpp>
+#include <Library/XmlElementLib/NonVisualGroupShapeProperties.hpp>
 #include <Library/XmlElementLib/NotesSize.hpp>
 #include <Library/XmlElementLib/Override.hpp>
 #include <Library/XmlElementLib/Relationship.hpp>
+#include <Library/XmlElementLib/ShapeTree.hpp>
 #include <Library/XmlElementLib/SlideMasterId.hpp>
 #include <Library/XmlElementLib/SlideMasterList.hpp>
 #include <Library/XmlElementLib/SlideSize.hpp>
+#include <Library/XmlElementLib/Transform2D.hpp>
 
 #endif
