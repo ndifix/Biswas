f4bd8e8625a7ad7ac29773597e771eb03886d8a9
ndifix <yoshiki.linuxmint@gmail.com>
2023-11-06 21:44:19 +0900

    accent1Colorを追加

--- Include/Library/XmlElementLib/ColorScheme.hpp
+++ Include/Library/XmlElementLib/ColorScheme.hpp
@@ -2,11 +2,13 @@
 #define XML_ELEMENT_LIB_ColorScheme
 
 #include <Library/XmlBaseLib.hpp>
+#include <Library/XmlElementLib/Accent1Color.hpp>
 
 namespace xmlElm {
 
 class ColorScheme : public XmlElement {
 private:
+    Accent1Color *accent1Color;
 public:
     Attribute *Name;
     ColorScheme();
