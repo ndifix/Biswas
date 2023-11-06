8cd69cda864e4b7495eb4aefb14a1c773fcb0e2a
ndifix <yoshiki.linuxmint@gmail.com>
2023-11-06 20:43:12 +0900

    themeElementを追加

---
+++ Include/Library/XmlElementLib/ThemeElements.hpp
@@ -0,0 +1,16 @@
+#ifndef XML_ELEMENT_LIB_ThemeElements
+#define XML_ELEMENT_LIB_ThemeElements
+
+#include <Library/XmlBaseLib.hpp>
+
+namespace xmlElm {
+
+class ThemeElements : public XmlElement {
+private:
+public:
+    ThemeElements();
+};
+
+}
+
+#endif
