72c4a1803d1b419cc2fa31ea0300930ccd8be107
ndifix <yoshiki.linuxmint@gmail.com>
2023-11-06 20:59:30 +0900

    Themeにnameを追加

--- Library/XmlElementLib/Theme.cpp
+++ Library/XmlElementLib/Theme.cpp
@@ -5,6 +5,8 @@ using namespace xmlElm;
 Theme::Theme (
     ) : XmlRootElement("theme", xmlns::drawingm)
 {
+    this->Name = new Attribute("name", "");
+    this->AddAttribute(this->Name);
     this->themeElements = new ThemeElements();
     this->AddChildElement(this->themeElements);
 }
