62720cd5b311af0dfc333d95eba3e0bfe5e9b649
ndifix <yoshiki.linuxmint@gmail.com>
2023-11-07 18:37:12 +0900

    Dark2Colorを追加

--- Library/XmlElementLib/ColorScheme.cpp
+++ Library/XmlElementLib/ColorScheme.cpp
@@ -14,7 +14,9 @@ ColorScheme::ColorScheme (
     this->accent5Color = new Accent5Color();
     this->accent6Color = new Accent6Color();
     this->dark1Color = new Dark1Color();
+    this->dark2Color = new Dark2Color();
     this->AddChildElement(this->dark1Color);
+    this->AddChildElement(this->dark2Color);
     this->AddChildElement(this->accent1Color);
     this->AddChildElement(this->accent2Color);
     this->AddChildElement(this->accent3Color);
