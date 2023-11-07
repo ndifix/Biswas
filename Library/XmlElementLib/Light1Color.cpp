94cd30fdb83c80f176557a1ff124689f4b817468
ndifix <yoshiki.linuxmint@gmail.com>
2023-11-07 18:47:50 +0900

    Light1Colorを追加

---
+++ Library/XmlElementLib/Light1Color.cpp
@@ -0,0 +1,12 @@
+#include <Library/XmlElementLib.hpp>
+
+using namespace xmlElm;
+
+Light1Color::Light1Color (
+    ) : XmlElement("lt1", xmlns::drawingm)
+{
+    this->systemColor = new SystemColor();
+    this->systemColor->Val->val = "window";
+    this->systemColor->LastColor->val = "FFFFFF";
+    this->AddChildElement(this->systemColor);
+}
