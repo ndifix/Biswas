fe688ce7f0bb9d60ffc7c74eadd3f024d9405b46
ndifix <yoshiki.linuxmint@gmail.com>
2023-11-07 18:24:52 +0900

    systemColorを追加

--- Include/Library/XmlElementLib/Dark1Color.hpp
+++ Include/Library/XmlElementLib/Dark1Color.hpp
@@ -2,11 +2,13 @@
 #define XML_ELEMENT_LIB_Dark1Color
 
 #include <Library/XmlBaseLib.hpp>
+#include <Library/XmlElementLib/SystemColor.hpp>
 
 namespace xmlElm {
 
 class Dark1Color : public XmlElement {
 private:
+    SystemColor *systemColor;
 public:
     Dark1Color();
 };
