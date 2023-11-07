682bcec3156ffc814b031b5475e81b72cbf694ab
ndifix <yoshiki.linuxmint@gmail.com>
2023-11-07 18:24:52 +0900

    systemColorを追加

--- Library/XmlElementLib/Dark1Color.cpp
+++ Library/XmlElementLib/Dark1Color.cpp
@@ -5,4 +5,8 @@ using namespace xmlElm;
 Dark1Color::Dark1Color (
     ) : XmlElement("dk1", xmlns::drawingm)
 {
+    this->systemColor = new SystemColor();
+    this->systemColor->Val->val = "windowText";
+    this->systemColor->LastColor->val = "000000";
+    this->AddChildElement(this->systemColor);
 }
