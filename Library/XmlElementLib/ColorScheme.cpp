db14b327bc32a46aa1c2a80a4d4b14c02969f240
ndifix <yoshiki.linuxmint@gmail.com>
2023-11-07 17:34:33 +0900

    Accent2Colorを追加

--- Library/XmlElementLib/ColorScheme.cpp
+++ Library/XmlElementLib/ColorScheme.cpp
@@ -8,5 +8,7 @@ ColorScheme::ColorScheme (
     this->Name = new Attribute("name", "");
     this->AddAttribute(this->Name);
     this->accent1Color = new Accent1Color();
+    this->accent2Color = new Accent2Color();
     this->AddChildElement(this->accent1Color);
+    this->AddChildElement(this->accent2Color);
 }
