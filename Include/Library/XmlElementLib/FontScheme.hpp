79f9b6d7e63c042e5992e4a7fb71c6f57fe22031
ndifix <yoshiki.linuxmint@gmail.com>
2023-11-08 19:09:21 +0900

    MinorFontを追加

--- Include/Library/XmlElementLib/FontScheme.hpp
+++ Include/Library/XmlElementLib/FontScheme.hpp
@@ -3,12 +3,14 @@
 
 #include <Library/XmlBaseLib.hpp>
 #include <Library/XmlElementLib/MajorFont.hpp>
+#include <Library/XmlElementLib/MinorFont.hpp>
 
 namespace OpenXml::Drawing {
 
 class FontScheme : public XmlElement {
 private:
     MajorFont *majorFont;
+    MinorFont *minorFont;
 public:
     Attribute *Name;
     FontScheme();
---
+++ Library/XmlElementLib/MinorFont.cpp
@@ -0,0 +1,8 @@
+#include <Library/XmlElementLib.hpp>
+
+using namespace OpenXml::Drawing;
+
+MinorFont::MinorFont (
+    ) : XmlElement("minorFont", xmlns::drawingm)
+{
+}
