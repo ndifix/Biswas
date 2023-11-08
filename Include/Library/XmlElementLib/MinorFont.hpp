cf0179ae73a5c3a8b021be872a6f85a1a01151b9
ndifix <yoshiki.linuxmint@gmail.com>
2023-11-08 19:09:21 +0900

    MinorFontを追加

---
+++ Include/Library/XmlElementLib/MinorFont.hpp
@@ -0,0 +1,16 @@
+#ifndef XML_ELEMENT_LIB_MinorFont
+#define XML_ELEMENT_LIB_MinorFont
+
+#include <Library/XmlBaseLib.hpp>
+
+namespace OpenXml::Drawing {
+
+class MinorFont : public XmlElement {
+private:
+public:
+    MinorFont();
+};
+
+}
+
+#endif
