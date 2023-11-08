280de10f9cd3a5639ce0fc3522625e9e39c28388
ndifix <yoshiki.linuxmint@gmail.com>
2023-11-08 19:02:31 +0900

    MajorFontを追加

--- Include/Library/XmlElementLib/FontScheme.hpp
+++ Include/Library/XmlElementLib/FontScheme.hpp
@@ -2,11 +2,13 @@
 #define XML_ELEMENT_LIB_FontScheme
 
 #include <Library/XmlBaseLib.hpp>
+#include <Library/XmlElementLib/MajorFont.hpp>
 
 namespace OpenXml::Drawing {
 
 class FontScheme : public XmlElement {
 private:
+    MajorFont *majorFont;
 public:
     Attribute *Name;
     FontScheme();
---
+++ Library/XmlElementLib/MajorFont.cpp
@@ -0,0 +1,8 @@
+#include <Library/XmlElementLib.hpp>
+
+using namespace OpenXml::Drawing;
+
+MajorFont::MajorFont (
+    ) : XmlElement("majorFont", xmlns::drawingm)
+{
+}
