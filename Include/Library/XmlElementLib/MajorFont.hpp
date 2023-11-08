b28682a740cb10b7d2802e85dabf7dd61ec4eca0
ndifix <yoshiki.linuxmint@gmail.com>
2023-11-08 19:26:32 +0900

    LatinFontを追加

--- Include/Library/XmlElementLib/MajorFont.hpp
+++ Include/Library/XmlElementLib/MajorFont.hpp
@@ -2,11 +2,13 @@
 #define XML_ELEMENT_LIB_MajorFont
 
 #include <Library/XmlBaseLib.hpp>
+#include <Library/XmlElementLib/LatinFont.hpp>
 
 namespace OpenXml::Drawing {
 
 class MajorFont : public XmlElement {
 private:
+    LatinFont *latinFont;
 public:
     MajorFont();
 };
