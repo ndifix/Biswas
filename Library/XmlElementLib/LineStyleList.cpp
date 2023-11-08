a852dc369754c7c2ae11fe727f5492652f296b2d
ndifix <yoshiki.linuxmint@gmail.com>
2023-11-08 23:00:10 +0900

    Outlineを追加

--- Library/XmlElementLib/LineStyleList.cpp
+++ Library/XmlElementLib/LineStyleList.cpp
@@ -5,4 +5,7 @@ using namespace OpenXml::Drawing;
 LineStyleList::LineStyleList (
     ) : XmlElement("lnStyleLst", xmlns::drawingm)
 {
+    this->AddChildElement(new Outline());
+    this->AddChildElement(new Outline());
+    this->AddChildElement(new Outline());
 }
