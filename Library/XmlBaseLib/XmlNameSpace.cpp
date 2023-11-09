7bdfbc210d1f71d352c97ee0ccda59cb6fd0f42f
ndifix <yoshiki.linuxmint@gmail.com>
2023-11-09 15:39:17 +0900

    relファイルの名前空間を修正

--- Library/XmlBaseLib/XmlNameSpace.cpp
+++ Library/XmlBaseLib/XmlNameSpace.cpp
@@ -6,15 +6,18 @@ namespace xmlns {
 
 const char emptystrSign = ' ';
 const char content_Sign = 'c';
+const char relpack_Sign = 'x';
 const char drawingmSign = 'a';
 const char relationSign = 'r';
 const char presentaSign = 'p';
 const char *content_NS = "http://schemas.openxmlformats.org/package/2006/content-types";
+const char *relpack_NS = "http://schemas.openxmlformats.org/package/2006/relationships";
 const char *drawingmNS = "http://schemas.openxmlformats.org/drawingml/2006/main";
 const char *relationNS = "http://schemas.openxmlformats.org/officeDocument/2006/relationships";
 const char *presentaNS = "http://schemas.openxmlformats.org/presentationml/2006/main";
 
 const XmlNameSpace content_ = {content_Sign, content_NS};
+const XmlNameSpace relpack_ = {relpack_Sign, relpack_NS};
 const XmlNameSpace drawingm = {drawingmSign, drawingmNS};
 const XmlNameSpace relation = {relationSign, relationNS};
 const XmlNameSpace presenta = {presentaSign, presentaNS};
