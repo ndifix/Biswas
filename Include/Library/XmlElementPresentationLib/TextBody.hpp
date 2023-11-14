3cf22bc75ca68ad570f3ff8fa46cf6547a3a23d5
ndifix <yoshiki.linuxmint@gmail.com>
2023-11-14 19:21:10 +0900

    BodyPropertiesを追加

--- Include/Library/XmlElementPresentationLib/TextBody.hpp
+++ Include/Library/XmlElementPresentationLib/TextBody.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
 #include <Library/XmlBaseLib.hpp>
+#include <Library/XmlElementDrawingLib/BodyProperties.hpp>
 
 namespace OpenXml::Presentation {
 
 class TextBody : public XmlElement {
 private:
 public:
+    Drawing::BodyProperties *bodyProperties;
+
     TextBody();
 };
 
